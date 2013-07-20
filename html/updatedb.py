2c29b446726775db829aa9dbd3ee38f07ae23de5
Will Wade <willwade@gmail.com>
2013-07-21 00:54:50 +0100

    updated now removing the optional elements

--- html/updatedb.py
+++ html/updatedb.py
@@ -20,13 +20,13 @@ tdir = "../templates/"
 items = os.listdir(tdir)
 
 types = []
+alloptionals = []
 # get types and update those 
 for file in items:
     if file.startswith("Type_") and file.endswith(".xml"):
         tree = etree.parse(tdir+file)
         name= tree.findtext("name")
         shortname = os.path.splitext(file)[0][5:]
-        # we'll use this later..
         types.append(shortname)
         optionals= tree.findtext("optionals").split(',')
         if len(optionals)>0:
@@ -36,6 +36,8 @@ for file in items:
                 n=1
                 for option in optionals:
                     if (len(option)>1):
+                        # we'll use this later..
+                        alloptionals.append(option)
                         #get the desc for the optional
                         otree = etree.parse(tdir+option+'.xml')
                         optwriter.writerow([n, otree.findtext("name"), option])
@@ -51,13 +53,14 @@ for file in items:
         type= tree.findtext("type")
         #case= tree.findtext("case")
         shortname = os.path.splitext(file)[0]
-        #print name, type, case, shortname
-        id=getIDforDesign(type)
-        with open('db/Type'+type+'.csv', 'a') as csvfile:
-            optwriter = csv.writer(csvfile)
-            if (id==0):
-                optwriter.writerow(['id', 'desc', 'xml'])
-            optwriter.writerow([id+1, name, shortname])
-            csvfile.close()
+        if shortname not in alloptionals:
+            #print name, type, case, shortname
+            id=getIDforDesign(type)
+            with open('db/Type'+type+'.csv', 'a') as csvfile:
+                optwriter = csv.writer(csvfile)
+                if (id==0):
+                    optwriter.writerow(['id', 'desc', 'xml'])
+                optwriter.writerow([id+1, name, shortname])
+                csvfile.close()
 
         
