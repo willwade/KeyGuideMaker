4b2da7e487d9428b5167245183d89948915de52a
Will Wade <willwade@gmail.com>
2013-08-01 00:14:03 +0100

    added some other bits. hopefully nothing broken

--- GridTemplater.py
+++ GridTemplater.py
@@ -10,12 +10,13 @@
 import argparse
 import logging
 import os
+import sys
 # Needs https://pypi.python.org/pypi/svgwrite/
 try:
     import svgwrite
 except ImportError:
     print "svgwrite needs to be installed. See https://pypi.python.org/pypi/svgwrite/"
-import sys 
+
 # for the XML parsing cETree is faster
 try:
     import xml.etree.cElementTree as ET
@@ -27,13 +28,25 @@ def convertpxtomm(pixels,dpi):
      spixels_mm=(pixels*25.4)/dpi
      return spixels_mm
 
-def createGrid(w,h,cellspacing,x,y,linewidth,fname='output',linecurved=True):
+def getDeviceDetails(type):
+    tree = ET.parse(__location__+"/templates/Type_"+type+".xml")
+    doc = tree.getroot()
+    return {'winwidth':int(doc.find('winwidth').text), 'winheight':int(doc.find('winheight').text) ,'dpi': int(doc.find('dpi').text)}  
+
+
+def createGrid(type,dpi,w,h,cellspacing,spacingunits,x,y,linewidth,fname='output',linecurved=True):
     # ok lets do some math!
     # whats the cell spacing? NB: this is mm - in the grid its pixels so  this needs converting
     #cellspacing = convertpxtomm(8,264)
     # line width of the laser cutter
     linewidth = 0.01
-    cellspacing_mm = convertpxtomm(cellspacing,132)
+    if spacingunits == 'px':
+        cellspacing_mm = convertpxtomm(cellspacing,132)
+    else:
+        cellspacing_mm = cellspacing
+    
+    # print 'cellspacing_mm', cellspacing_mm, 'w:', w, 'h:',h, 'x:', x, 'y:', y
+    
     cellwidth = ((w-(cellspacing_mm*x))/x)
     cellheight = ((h-(cellspacing_mm*y))/y)
     #print 'cellspacing', cellspacing, 'width', cellwidth, 'height', cellheight
@@ -67,14 +80,17 @@ def createGrid(w,h,cellspacing,x,y,linewidth,fname='output',linecurved=True):
     dwg.save()
     
 
-parser = argparse.ArgumentParser(prog='GridTemplater',description="Creates bases of keyguard designs from some basic settings. Use like: ./GridTemplater.py --cellwidth 7 --cellheight 5 --winwidth 198 --winheight 148 --cellspacing 1.705")
+parser = argparse.ArgumentParser(prog='GridTemplater',description="Creates bases of keyguard designs from some basic settings. Use like: ./GridTemplater.py --cellwidth 7 --cellheight 5 --winwidth 198 --winheight 148 --cellspacing 1.705  or ./GridTemplater.py --type iPad --cellwidth 7 --cellheight 5 --cellspacing 8 -u px")
 # Basics
 parser.add_argument('--curved','-c', type=bool, default=True, help='Do you want curved guide holes?')
-parser.add_argument('--cellwidth', type=int, default=7, help='The number of cells across')
+parser.add_argument('--cellwidth', type=int, help='The number of cells across')
+parser.add_argument('--type', '-t', type=str, default='', help='Provide a device type and you don\'t need to provide winwidth, winheight or dpi')
 parser.add_argument('--cellheight', type=int, default=5, help='The number of cells up')
-parser.add_argument('--winwidth', type=int, default=198, help='The width of the aperture (window) in mm')
-parser.add_argument('--winheight', type=int, default=148, help='The height of the aperture (window) in mm')
+parser.add_argument('--winwidth', type=int, help='The width of the aperture (window) in mm')
+parser.add_argument('--winheight', type=int, help='The height of the aperture (window) in mm')
+parser.add_argument('--dpi','-d', type=int, help='Dots per inch. DPI')
 parser.add_argument('--cellspacing','-s', type=float, default=8, help='Cell Spacing (in pixels)')
+parser.add_argument('--spacingunits','-u', type=str, default='px', help='Cell Spacing units (px or mm)')
 parser.add_argument('--linewidth', type=float, default=0.01, help='Width of the lines to draw (in mm)')
 parser.add_argument('--filename','-f', type=str, default="output", help='Name of the final  image that gets created')
 parser.add_argument('--logfile','-l', type=str, default="", help='Logfile location. NB: If blank no log created')      
@@ -85,6 +101,11 @@ args = parser.parse_args()
 #Fix the location if called elsewhere
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-# here we go. This is a monster
+if args.type !='':
+    typedets = getDeviceDetails(args.type)
+    args.dpi = typedets['dpi']
+    args.winwidth = typedets['winwidth']
+    args.winheight = typedets['winheight']
 
-createGrid(args.winwidth,args.winheight,args.cellspacing,args.cellwidth,args.cellheight,args.linewidth,args.filename,args.curved)
+# here we go. This is a monster    
+createGrid(args.type,args.dpi,args.winwidth,args.winheight,args.cellspacing,args.spacingunits,args.cellwidth,args.cellheight,args.linewidth,args.filename,args.curved)
